df048a95c81f6759233322c1ebfdf16d98f63ccd
DaiNguyen <nguyenddai99@gmail.com>
2018-09-28 13:40:03 +0700

    add insertFromFile method

--- DictionaryManagement.java
+++ DictionaryManagement.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+import java.io.*;
 public class DictionaryManagement {
     public static void insertFromCommandline(Dictionary dict) {
         Scanner scanner = new Scanner(System.in);
@@ -9,6 +9,32 @@ public class DictionaryManagement {
             String word_explain = scanner.nextLine();
             Word newWord = new Word(word_target, word_explain);
             dict.insertWord(newWord);
+        }
+    }
+
+    public static void insertFromFile(Dictionary dict) throws IOException {
+        FileReader input = null;
+        try {
+            input = new FileReader("D:/Java Project/OOPBigProject/src/dictionary.txt");
+            char ch, tab  = '\t', endln = '\n', endf = (char)(-1);
+            String word_target, word_explain;
+            while(true) {
+                word_target = "";
+                word_explain = "";
+                while ((ch = (char) input.read()) != tab) {
+                    word_target = word_target + ch;
+                }
+                while ((ch = (char) input.read()) != endln && ch != endf) {
+                    word_explain = word_explain + ch;
+                }
+                dict.insertWord(word_target, word_explain);
+                if(ch == endf)
+                    break;
+            }
+        }finally
+        {
+                if(input != null)
+                    input.close();
         }
     }
 }
