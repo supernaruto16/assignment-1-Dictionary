44fced7c7264f3fa8cdb60445e09a1ce03dc516d
QuangVu <supernaruto16@gmail.com>
2018-10-13 20:55:49 +0700

    add ListView

--- DictionaryGraphic/src/sample/Main.java
+++ DictionaryGraphic/src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Storm Dictionary");
-        primaryStage.setScene(new Scene(root, 683, 384));
+        primaryStage.setScene(new Scene(root, 709, 493));
         primaryStage.show();
     }
 
